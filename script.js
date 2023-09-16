7cbd789c87824985acad471cdcda54a02983d12d
Dominik Stefancic <119432963+ImBoB99@users.noreply.github.com>
2023-09-16 11:35:13 +0200

    Add custom grid sizing Verify correct grid sizing input Set default grid size to 16x16

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 const container = document.querySelector(".container");
-
-
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-}
+const gridDimensions = 800;
+let squareDimensions = 0;
 
 function hoverPaint(e) {
     e.target.classList.add("paint");
 }
 
-const squares = document.querySelectorAll(".square");
+function gridSize() {
+    let squaresPerSide = prompt("Enter grid size");
+    // convert string to number
+    squaresPerSide = parseInt(squaresPerSide);
 
-squares.forEach(square => square.addEventListener("mouseover", hoverPaint));
+    if (isNaN(squaresPerSide) || squaresPerSide < 0 || squaresPerSide > 100) {
+        alert("Please enter a valid number between 1 and 100");
+        squaresPerSide = 16;
+    }
+    setGrid(squaresPerSide);
+}
+
+function setGrid(squaresPerSide = 16) {
+    container.innerHTML = "";
+    for (let i = 0; i < (squaresPerSide**2); i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        squareDimensions = gridDimensions / squaresPerSide;
+        square.setAttribute("style", `width:${squareDimensions}px; height:${squareDimensions}px;`)
+        container.appendChild(square);
+        squareEvents();
+    }
+}
+
+function squareEvents () {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener("mouseover", hoverPaint));
+}
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", gridSize);
+
+setGrid();
