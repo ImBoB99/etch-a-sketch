d46cf183041d5ffe8ee61fe4a536c814dedf8468
Dominik Stefancic <119432963+ImBoB99@users.noreply.github.com>
2023-09-16 10:43:36 +0200

    Add square painting on hover

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
 
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
+    square.classList.add("square");
     container.appendChild(square);
 }
+
+function hoverPaint(e) {
+    e.target.classList.add("paint");
+}
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => square.addEventListener("mouseover", hoverPaint));
